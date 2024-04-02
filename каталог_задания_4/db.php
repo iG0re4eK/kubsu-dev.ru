69dc8a7d310aa85bae836e7775d9affe6ffb1a89
iG0re4eK <69710661+iG0re4eK@users.noreply.github.com>
2024-04-02 21:32:18 +0300

    return db.php 3

--- каталог_задания_4/db.php
+++ каталог_задания_4/db.php
@@ -45,7 +45,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
     if (!empty($errors)) {
-        setcookie('form_errors', serialize($errors), 0 , '/');
+        setcookie('form_errors', serialize($errors), time() + (365 * 24 * 60 * 60), '/');
         header('Location: ' . $_SERVER['PHP_SELF']);
         exit;
     } else {
